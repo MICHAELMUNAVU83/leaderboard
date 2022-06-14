2c5db2134a9a96516e4717b50cdf6b632786998a
trance <michaelmunavu83@gmail.com>
2022-06-14 18:40:20 +0300

    Added asynch and await js

--- src/html-markup.js
+++ src/html-markup.js
@@ -1,35 +1,63 @@
-const boards = [
-  {
-    name: 'Name',
-    score: 100,
-  },
-  {
-    name: 'Name',
-    score: 340,
-  },
-  {
-    name: 'Name',
-    score: 60,
-  },
-  {
-    name: 'Name',
-    score: 80,
-  },
-  {
-    name: 'Name',
-    score: 10,
-  },
-];
 
-boards.forEach((board) => {
-  document.querySelector('.score-spans').innerHTML += `
-        <div class="each-score">
-                        <span>${board.name} :</span><span>${board.score}</span>
-    
-                    </div>
-        
-        
-        `;
-});
+class Store {
+  constructor(user, score) {
+    this.user = user;
+    this.score = score;
+  }
+}
+document.getElementById('submit-button').addEventListener('click', async (e)=>{
+  e.preventDefault()
+  const uservalue = document.getElementById('userinput').value
+  const scorevalue = document.getElementById('score-input').value
+ 
+  document.getElementById('score-input').value = ' '
+  document.getElementById('userinput').value = ' '
+ 
 
-localStorage.setItem('Names and scores', JSON.stringify(boards));
+const newScore = new Store(uservalue , scorevalue);
+
+
+const content = await fetch(
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoituyoiM55uY/scores/",
+  {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newScore),
+  }
+)
+
+})
+
+
+
+
+
+document.getElementById('refresh').addEventListener('click',async (e)=>{
+  e.preventDefault()
+  document.querySelector(".score-spans").innerHTML = ''
+
+
+const request = await fetch(
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoituyoiM55uY/scores/"
+);
+ 
+const data = await request.json()
+const getContent = async (data)=>{
+  (data.result).forEach((board) => {
+    document.querySelector(".score-spans").innerHTML += `
+          <div class="each-score">
+                          <span>${board.user} :</span><span>${board.score}</span>
+      
+                      </div>
+          
+          
+          `;
+  });
+
+}
+getContent(data)
+
+
+
+})
+
