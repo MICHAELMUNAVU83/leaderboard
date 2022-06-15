812a3ad1352330c0ff2fea033b3fee7d0525598a
trance <michaelmunavu83@gmail.com>
2022-06-15 13:43:04 +0300

    Add new js to show result on refresh

--- src/html-markup.js
+++ src/html-markup.js
@@ -4,41 +4,60 @@ class Store {
     this.score = score;
   }
 }
-document.getElementById('submit-button').addEventListener('click', async (e) => {
+document
+  .getElementById("submit-button")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    const uservalue = document.getElementById("userinput").value;
+    const scorevalue = document.getElementById("score-input").value;
+
+    document.getElementById("score-input").value = " ";
+    document.getElementById("userinput").value = " ";
+
+    const newScore = new Store(uservalue, scorevalue);
+
+    const content = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoiyuhoiM55uY/scores/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newScore),
+      }
+    );
+  });
+
+document.getElementById("refresh").addEventListener("click", async (e) => {
   e.preventDefault();
-  const uservalue = document.getElementById('userinput').value;
-  const scorevalue = document.getElementById('score-input').value;
-
-  document.getElementById('score-input').value = ' ';
-  document.getElementById('userinput').value = ' ';
-
-  const newScore = new Store(uservalue, scorevalue);
-
-  const content = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoituyoiM55uY/scores/',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newScore),
-    },
-
-  );
-  const afterContent = await content.json();
-  return afterContent;
-});
-
-document.getElementById('refresh').addEventListener('click', async (e) => {
-  e.preventDefault();
-  document.querySelector('.score-spans').innerHTML = '';
+  document.querySelector(".score-spans").innerHTML = "";
 
   const request = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoituyoiM55uY/scores/',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoiyuhoiM55uY/scores/"
   );
 
   const data = await request.json();
   const getContent = async (data) => {
-    (data.result).forEach((board) => {
-      document.querySelector('.score-spans').innerHTML += `
+    data.result.forEach((board) => {
+      document.querySelector(".score-spans").innerHTML += `
+          <div class="each-score">
+                          <span>${board.user} :</span><span>${board.score}</span>
+      
+                      </div>
+          
+          
+          `;
+    });
+  };
+  getContent(data);
+});
+window.addEventListener("load", async () => {
+  const request = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BTEFhiuCoituyoiM55uY/scores/"
+  );
+
+  const data = await request.json();
+  const getContent = async (data) => {
+    data.result.forEach((board) => {
+      document.querySelector(".score-spans").innerHTML += `
           <div class="each-score">
                           <span>${board.user} :</span><span>${board.score}</span>
       
